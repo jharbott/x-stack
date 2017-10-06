76afb83999823d4ac62b196d25198bb54467e959
Dean Troyer <dtroyer@gmail.com>
2017-10-06 09:31:35 -0500

    Configure keystone

--- x-keystone.sh
+++ x-keystone.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # x-keystone
 
+set -e
+
 source x-stackrc
 
 source ini-config.inc
@@ -8,6 +10,12 @@ source lib.inc
 
 KEYSTONE_CONF_DIR=/etc/keystone
 KEYSTONE_CONF=$KEYSTONE_CONF_DIR/keystone.conf
+
+# until we have https in
+KEYSTONE_SERVICE_PROTOCOL=http
+
+KEYSTONE_SERVICE_URI=${KEYSTONE_SERVICE_PROTOCOL}://${KEYSTONE_SERVICE_HOST}/identity
+KEYSTONE_AUTH_URI=$KEYSTONE_SERVICE_URI
 
 function _config_keystone_apache_wsgi {
     local keystone_apache_conf
@@ -36,6 +44,32 @@ function _config_keystone_apache_wsgi {
     " -i $keystone_apache_conf
 }
 
+function bootstrap_keystone {
+    sudo $BASE_BIN_DIR/keystone-manage bootstrap \
+        --bootstrap-username admin \
+        --bootstrap-password "$ADMIN_PASSWORD" \
+        --bootstrap-project-name admin \
+        --bootstrap-role-name admin \
+        --bootstrap-service-name keystone \
+        --bootstrap-region-id "$REGION_NAME" \
+        --bootstrap-admin-url "$KEYSTONE_AUTH_URI" \
+        --bootstrap-public-url "$KEYSTONE_SERVICE_URI"
+
+    # Create additional domains, projects, users and roles
+    # TODO: write clouds.yaml dynamically
+    openstack --os-cloud xstack-admin <<EOF
+        domain create --or-show $SERVICE_DOMAIN_NAME
+        project create --or-show --domain $SERVICE_DOMAIN_NAME $SERVICE_PROJECT_NAME
+        role create --or-show service
+        role create --or-show ResellerAdmin
+EOF
+}
+
+function cleanup_keystone {
+    # pass
+    delete_database keystone
+}
+
 function configure_keystone {
     # Be like DevStack
     iniset $KEYSTONE_CONF identity domain_specific_drivers_enabled "True"
@@ -45,7 +79,13 @@ function configure_keystone {
     iniset $KEYSTONE_CONF cache backend "dogpile.cache.memcached"
     iniset $KEYSTONE_CONF cache memcache_servers localhost:11211
 
+    iniset $KEYSTONE_CONF DEFAULT public_endpoint $KEYSTONE_SERVICE_URI
+    iniset $KEYSTONE_CONF DEFAULT admin_endpoint $KEYSTONE_AUTH_URI
+
     iniset $KEYSTONE_CONF database connection ${BASE_SQL_CONN}/keystone?charset=utf8
+    iniset $KEYSTONE_CONF token provider fernet
+
+    iniset $KEYSTONE_CONF DEFAULT logging_exception_prefix "%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s"
 
 	_config_keystone_apache_wsgi
 }
@@ -66,16 +106,39 @@ function install_keystone {
     echo "manual" > /etc/init/keystone.override
 
     install_package keystone python-keystoneauth1 python-keystoneclient python-keystonemiddleware apache2 libapache2-mod-wsgi memcached python-memcache
-	sudo a2enmod wsgi
-	restart_service apache2
+    rm -f /var/lib/keystone/keystone.db
 }
 
-install_keystone
-configure_keystone
-init_keystone
+function start_keystone {
+    sudo a2enmod wsgi
+    sudo a2ensite keystone
+    restart_service apache2
+}
 
-# Bootstrap Keystone
+function stop_keystone {
+    sudo a2dissite keystone
+    restart_service apache2
+}
 
-# Init fernet
+ACTION=${1:-none}
+case $ACTION in
+    stack)
+        install_keystone
+        configure_keystone
+        init_keystone
+        start_keystone
+        bootstrap_keystone
+        ;;
+    start)
+        start_keystone
+        ;;
+    stop|unstack)
+        stop_keystone
+        ;;
+    clean)
+        stop_keystone
+        cleanup_keystone
+        ;;
+esac
 
-
+echo "$0 Fini"
